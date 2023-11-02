6d33aec21bd8a431e0f7515d3665b5e7051f250f
Darren Jensen <darren.jensen@gmail.com>
2023-11-02 14:25:39 +0700

    added InvariantExample and ERC20InterfaceId tests

---
+++ test/ERC20InterfaceId.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {ERC20InterfaceId} from "../src/ERC20InterfaceId.sol";
+
+contract ERC20InterfaceIdTest is Test {
+    ERC20InterfaceId public iface;
+
+    function setUp() public {
+        iface = new ERC20InterfaceId();
+    }
+
+    function test_getERC20InterfaceId() public {
+        bytes4 ifaceId = iface.getERC20InterfaceId();
+        console2.logBytes4(ifaceId);
+        assertEq(ifaceId, "");
+    }
+}
---
+++ test/InvariantExample.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {InvariantExample} from "../src/InvariantExample.sol";
+
+contract InvariantExample1Test is Test {
+
+    InvariantExample foo;
+
+    function setUp() external {
+        foo = new InvariantExample();
+    }
+
+    function invariant_A() external {
+        assertEq(foo.val1() + foo.val2(), foo.val3());
+    }
+
+    function invariant_B() external {
+        assertGe(foo.val1() + foo.val2(), foo.val1());
+    }
+}
