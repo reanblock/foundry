6c1dbc1ea59acea2ed4ae82070da33d7daa53f9f
Darren Jensen <darren.jensen@gmail.com>
2023-12-29 09:39:08 +0700

    update deploy script

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,11 +2,16 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
+import {Counter} from "../src/Counter.sol";
+
+// forge script script/Counter.s.sol:CounterScript --rpc-url=http://localhost:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --broadcast
 
 contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+        new Counter();
+        vm.stopBroadcast();
     }
 }
