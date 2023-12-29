f47ce965b145b30bc5ac83586583467c34991bd9
Darren Jensen <darren.jensen@gmail.com>
2023-12-29 10:42:54 +0700

    added test

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -5,6 +5,8 @@ import {Script, console2} from "forge-std/Script.sol";
 import {Counter} from "../src/Counter.sol";
 
 // forge script script/Counter.s.sol:CounterScript --rpc-url=http://localhost:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --broadcast
+// cast st 0x68b1d87f95878fe05b998f19b66f4baba5de1aed 0
+// cast send 0x68b1d87f95878fe05b998f19b66f4baba5de1aed "increment()" --private-key=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 
 
 contract CounterScript is Script {
     function setUp() public {}
