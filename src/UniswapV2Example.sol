a952dece59efba6d76830a30b558f9da43b1fbdf
Darren Jensen <darren.jensen@gmail.com>
2024-06-08 11:33:17 +0700

    added start of uniswap v2 example

---
+++ src/UniswapV2Example.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+import {IUniswapV2Router02} from '@uniswap/periphery/contracts/interfaces/IUniswapV2Router02.sol';
+
+contract UniswapV2Example {
+
+}
